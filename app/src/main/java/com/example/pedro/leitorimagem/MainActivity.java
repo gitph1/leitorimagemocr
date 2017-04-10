1ca97aa9a75c7eea19effaa7d0a09c3ecf6cdadb
Pedro <ph_fiorentin@terra.com.br>
2017-04-10 10:37:55 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/pedro/leitorimagem/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.pedro.leitorimagem;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.R.attr.bitmap;
+import static android.R.attr.rotation;
+import static android.R.attr.width;
+import static com.example.pedro.leitorimagem.R.attr.height;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button btnProcess;
+    EditText txtResult;
+    Button takePictureButton;
+    Uri file;
+    Bitmap bitmop;
+    Bitmap adjustedBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        takePictureButton = (Button) findViewById(R.id.button_image);
+        imageView = (ImageView)findViewById(R.id.image_view);
+        btnProcess = (Button)findViewById(R.id.button_process);
+        txtResult = (EditText)findViewById(R.id.textview_result);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+
+        final Bitmap bitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.test_image
+        );
+        //imageView.setImageBitmap(bitmap);
+
+        btnProcess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                if(!textRecognizer.isOperational()){
+                    Log.w("Error", "Detector dependencies are not yet available");
+
+                }else{
+                    Frame frame = new Frame.Builder().setBitmap(adjustedBitmap).build();
+                    SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for(int i = 0; i < items.size(); i++){
+                        TextBlock item = items.valueAt(i);
+                        stringBuilder.append(item.getValue());
+                        stringBuilder.append("\n");
+                    }
+                    txtResult.setText(stringBuilder.toString(), TextView.BufferType.EDITABLE);
+                    txtResult.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                takePictureButton.setEnabled(true);
+            }
+        }
+    }
+
+    public void takePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+        startActivityForResult(intent, 100);
+
+    }
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d("CameraDemo", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+
+                try {
+                    bitmop = MediaStore.Images.Media.getBitmap(this.getContentResolver(), file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    ExifInterface exif = new ExifInterface(file.getPath());
+                    int rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    int rotationInDegrees = exifToDegrees(rotation);
+                    Matrix matrix = new Matrix();
+                    if (rotation != 0f) {matrix.preRotate(rotationInDegrees);}
+                    adjustedBitmap = Bitmap.createBitmap(bitmop, 0, 0, bitmop.getWidth(), bitmop.getHeight(), matrix, true);
+                    imageView.setImageBitmap(adjustedBitmap);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+
+
+                //imageView.setImageURI(file);
+            }
+        }
+    }
+    private static int exifToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) { return 90; }
+        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {  return 180; }
+        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {  return 270; }
+        return 0;
+    }
+    /*public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }*/
+}
